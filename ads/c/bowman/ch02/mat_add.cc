ac7399fabe8e48df2bac2d4eb55fdcf1a6b486a3
Igor Dimitrov <igor-dimi@protonmail.com>
2024-02-12 23:34:43 +0100

    added matrix multiplication to bowman ch02

--- ads/c/bowman/ch02/mat_add.cc
+++ ads/c/bowman/ch02/mat_add.cc
@@ -1,11 +1,12 @@
-void mat_add(int rows, int cols, int** a, int** b, int** res)
+#include "mat.hh"
+void mat_add(int a[ROWS][COLS], int b[ROWS][COLS], int res[ROWS][COLS])
 {
 
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
             res[i][j] = 0;
 
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
             res[i][j] = a[i][j] + b[i][j];
 }
---
+++ ads/c/bowman/ch02/mat_mult.cc
@@ -0,0 +1,12 @@
+#include "mat.hh"
+
+void mat_mult(int a[ROWS][COLS], int b[ROWS][COLS], int res[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++){
+            res[i][j] = 0;
+            for (int k = 0; k < COLS; k++)
+                res[i][j] += a[i][k] * b[k][j];
+        }
+}
+
