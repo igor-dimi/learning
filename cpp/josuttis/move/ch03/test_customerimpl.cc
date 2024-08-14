5f973d99160b08488cda86c70121e79dec0c5f3b
Igor Dimitrov <igor-dimi@protonmail.com>
2024-08-14 12:56:51 +0200

    josuttis move semantics ch2 & ch3 notes

--- cpp/josuttis/move/ch03/test_customerimpl.cc
+++ cpp/josuttis/move/ch03/test_customerimpl.cc
@@ -4,18 +4,27 @@
 #include <vector>
 #include <algorithm>
 
+void foo(std::string&& arg)
+{
+    std::cout << arg << std::endl;
+}
+
+
+
 int main(int argc, char const *argv[])
 {
     std::vector<Customer> coll;
-    for (int i = 0; i <  12; i++) {
+    for (int i = 12; i > 0; i--) {
         coll.push_back(Customer{"TestCustomer " + std::to_string(i - 5)});
     }
+
 
     std::cout << "--- sort(): \n";
     std::sort(coll.begin(), coll.end(), [] (
        const Customer& c1, const Customer& c2) {
         return c1.getName() < c2.getName();
        });
+
     
     return 0;
 }
