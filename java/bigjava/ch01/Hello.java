d1f3efd33cbb6ed2eb4ccef852a2c19d4bf4013c
Igor Dimitrov <igor-dimi@protonmail.com>
2024-10-18 00:43:43 +0200

    learning big java cay horstmal ch01 ch02 files

--- java/bigjava/ch01/Hello.java
+++ java/bigjava/ch01/Hello.java
@@ -1,7 +1,17 @@
+import mypack.B;
 public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println(args[0] + " " + args[1]);
+        var greeting = "hello";
+        System.out.println(greeting);
+        System.out.println("hello".length());
+        System.out.println("hello".toUpperCase());
+        var res = greeting.replace("hell", "h");
+        System.out.println(greeting);
+        System.out.println(res);
+        A a = new A();
+        B b = new B();
+        // Rectangle box = new Rectangle(1, 2, 3, 4);
     }
 }
---
+++ java/bigjava/ch01/mypack/B.java
@@ -0,0 +1,8 @@
+package mypack;
+
+public class B {
+    public static void main(String[] args) {
+        C c = new C();
+    }
+    
+}
