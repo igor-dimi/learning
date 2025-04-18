bbfc5d48b24f25d76e3d4d13c5c95b9fc067ef39
Igor Dimitrov <igor-dimi@protonmail.com>
2025-04-18 15:28:22 +0200

    add gries ch03 notes, birthday calculations

---
+++ prolog/bramer/ch01/animals.pl
@@ -0,0 +1,8 @@
+dog(fido).
+dog(rover).
+dog(henry).
+cat(felix).
+cat(michael).
+cat(jane).
+animal(X) :- dog(X).
+% animal(X) :- cat(X).
---
+++ sml/reade/ch01/programs.sml
@@ -0,0 +1,2 @@
+fun p(n, r) = if n = 0
+              then 
